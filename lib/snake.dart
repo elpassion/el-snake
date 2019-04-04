e0d505bb345fc4c2f056ac722be2250b1bac7531
Paweł Gajda <pawegio@gmail.com>
2019-04-04 13:26:09 +0200

    Update velocity and color circles

--- lib/snake.dart
+++ lib/snake.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 import 'dart:ui';
 
 import 'package:el_snake/snake-game.dart';
+import 'package:flutter/material.dart';
 
 class Snake {
   final SnakeGame game;
@@ -12,16 +13,20 @@ class Snake {
 
   Snake(this.game, double x, double y) {
     points = [Point(x, y)];
-    velocity = Point(100, 0);
+    velocity = Point(0, 0);
     paint.color = Color(0xffff0000);
   }
 
   void render(Canvas canvas) {
-    points.forEach((Point<double> point) =>
-        canvas.drawCircle(Offset(point.x, point.y), 16, paint));
+    for (var i = 0; i < points.length - 1; i++) {
+      var hue = (i.toDouble() * 5) % 360;
+      paint.color = HSLColor.fromAHSL(1.0, hue, 0.5, 0.5).toColor();
+      canvas.drawCircle(Offset(points[i].x, points[i].y), 16, paint);
+    }
   }
 
   void update(double t) {
+    velocity = Point(0, game.force.y * 10);
     var head = points.last;
     var newHead = Point(head.x + velocity.x * t, head.y + velocity.y * t);
 
