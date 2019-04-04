64332193fd46eb903162c5df7d069857078eac89
Dariusz Rybicki <darrarski@gmail.com>
2019-04-04 14:51:24 +0200

    Fix iOS app

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -7,7 +7,6 @@
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
-  [FIRApp configure];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
