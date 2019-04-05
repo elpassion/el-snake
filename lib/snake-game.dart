c743fcc82c2e00bb71fd0766fffcf0242c7ae916
Paweł Gajda <pawegio@gmail.com>
2019-04-05 14:59:39 +0200

    Create circles

--- lib/snake-game.dart
+++ lib/snake-game.dart
@@ -5,16 +5,17 @@ import 'package:el_snake/circle.dart';
 import 'package:el_snake/explosion.dart';
 import 'package:el_snake/firebase-client.dart';
 import 'package:el_snake/force.dart';
+import 'package:el_snake/material.dart';
 import 'package:el_snake/random-string.dart';
 import 'package:el_snake/restart-button.dart';
 import 'package:el_snake/snake.dart';
 import 'package:el_snake/world.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class SnakeGame extends Game {
-  final forceFactor = 50;
+  final forceFactor = 100;
   final collectionPath = "game";
 
   Size screenSize;
@@ -25,7 +26,7 @@ class SnakeGame extends Game {
   List<Circle> circles = [];
   List<Explosion> explosions = [];
   FirebaseClient client;
-  int magicCounter = 20;
+//  int magicCounter = 20;
 
   Point<double> get center =>
       Point(screenSize.width / 2, screenSize.height / 2);
@@ -38,7 +39,8 @@ class SnakeGame extends Game {
     resize(await Flame.util.initialDimensions());
     world = World(center, 750.0);
     snake = Snake(RandomString(16), this, center);
-    client = FirebaseClient(this, snake);
+//    client = FirebaseClient(this, snake);
+    createCircles();
     restartButton = RestartButton(center);
   }
 
@@ -61,19 +63,20 @@ class SnakeGame extends Game {
 
   void update(double t) {
     updateCamera();
-    if (!snake.isDead && (collidesWithWorldEdge() || collidesWithDeadlyCircle())) {
+    if (!snake.isDead &&
+        (collidesWithWorldEdge() || collidesWithDeadlyCircle())) {
       snake.isDead = true;
       explosions.add(Explosion(snake.head.center));
     }
     world.update(t);
     snake.update(t);
     explosions.forEach((Explosion explosion) => explosion.update(t));
-    if (magicCounter <= 0) {
+    /*if (magicCounter <= 0) {
       magicCounter = 20;
       client.updateMyCircles();
     } else {
       magicCounter--;
-    }
+    }*/
   }
 
   bool collidesWithDeadlyCircle() {
@@ -109,5 +112,22 @@ class SnakeGame extends Game {
   bool collidesWithWorldEdge() {
     var distanceToCenter = snake.head.center.distanceTo(world.center);
     return distanceToCenter + snake.radius >= world.radius - world.stroke / 2;
+  }
+
+  void createCircles() {
+    var random = Random();
+    for (var i = 0; i < 70; i++) {
+      var radius = random.nextInt(20) + 10;
+      var x = random.nextInt(world.radius.toInt() * 2) - center.x;
+      var y = random.nextInt(world.radius.toInt() * 2) - center.y;
+      Point(random.nextDouble() * 100 - 50, random.nextDouble() * 100 - 50);
+      var hue = random.nextInt(360).toDouble();
+      circles.add(Circle(
+          Point(x, y),
+          radius.toDouble(),
+          Material(1.0, hue, 1.0, 0.5, true, 0),
+          Material(1.0, hue, 1.0, 0.5, true, 3),
+          RandomString(5)));
+    }
   }
 }
